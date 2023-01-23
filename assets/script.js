fb079516ef46edb1fe804958d4126dfcdf69d679
Joseph <117396146+joeytraffic@users.noreply.github.com>
2023-01-22 18:44:41 -0700

    fixed up the quiz section

--- assets/script.js
+++ assets/script.js
@@ -33,16 +33,64 @@ var answer2d = "<jscript>";
 var answer3a = 'alert(What is your name?)';
 var answer3b = 'prompt("What is your name?")';
 var answer3c = 'form("What is your name?")';
-var answer3d = 'prompt(What is your name?)'; 
+var answer3d = 'prompt(What is your name?)';
 
 var answer4a = "grabbing";
 var answer4b = "pulling";
 var answer4c = "hoisting";
-var answer4d = "yoinking"; 
+var answer4d = "yoinking";
+
+
+var submit = document.querySelector("#submit");
 
 
 
-var initials = document.querySelector("#name");
+
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+
+var newItem = document.querySelector(".players");
+
+function renderLeaderboard() {
+    var initials = localStorage.getItem("initials");
+    var score = localStorage.getItem("score")
+
+    if (!initials || !score) {
+        return;
+    }
+
+    var player = [initials, score];
+    function addPlayer() {
+        li1.textContent = "Player: " + player[0] + "  |  Score: " + player[1];
+        newItem.appendChild(li1);
+    }
+
+
+    addPlayer();
+}
+
+renderLeaderboard();
+
+submit.addEventListener("click", function (event) {
+    event.preventDefault();
+    var initials = document.querySelector("#name").value;
+    localStorage.setItem("initials", initials);
+    localStorage.setItem("score", score);
+    console.log(initials);
+    console.log(score);
+    var player = [initials, score];
+    console.log(player)
+    showHighScores();
+    function addPlayer() {
+        li1.textContent = "Player: " + player[0] + "  |  Score: " + player[1];
+        newItem.appendChild(li1);
+    }
+    document.getElementById("form").style.display = "none";
+}
+)
+
 
 
 function youWin() {
@@ -50,71 +98,88 @@ function youWin() {
     document.getElementById("answers").style.display = "none";
     document.getElementById("question").style.display = "none";
     document.getElementById("startQuiz").style.display = "none";
-    document.getElementById("win-form").style.display = "flex";
+    document.getElementById("form").style.display = "flex";
     document.getElementById("THE-LINE").style.display = "none";
-    console.log(initials.value);
 }
 
 function youLose() {
     document.getElementById("answers").style.display = "none";
     document.getElementById("question").style.display = "none";
     document.getElementById("startQuiz").style.display = "none";
-    document.getElementById("lose-form").style.display = "flex";
+    document.getElementById("form").style.display = "flex";
     document.getElementById("THE-LINE").style.display = "none";
 }
 
 
 function startTimer() {
     timerElement.textContent = 20;
-    timer = setInterval(function() {
-      timerCount--;
-      timerElement.textContent = timerCount;
-      if (timerCount >= 0) {
-        if (isWin && timerCount > 0) {
-          clearInterval(timer);
-          youWin();
+    timer = setInterval(function () {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount >= 0) {
+            if (isWin && timerCount > 0) {
+                clearInterval(timer);
+                youWin();
+            }
         }
-      }
-      if (timerCount <= 0) {
-        clearInterval(timer);
-        youLose();
-      }
+        if (timerCount <= 0) {
+            clearInterval(timer);
+            youLose();
+        }
     }, 1000);
-  }
+}
 
 
 function wrongAnswer() {
-    // timerCount = timerCount - 5;
-    // timerElement.textContent = timerCount;
+    timerCount = timerCount - 5;
+    timerElement.textContent = timerCount;
+
 }
 
 function correctAnswer() {
-    score = score + 5;
+    score = score + timerCount + 5;
+    timerCount = timerCount + 5;
+    timerElement.textContent = timerCount;
 }
 
-function resetButton() {
-    card1.removeEventListener("click", wrongAnswer);
-    card2.removeEventListener("click", wrongAnswer);
-    card3.removeEventListener("click", wrongAnswer);
-    card4.removeEventListener("click", wrongAnswer);
+function advance1() {
+    correctAnswer();
+    wrongAnswer(); // This is to undo the adding of 5 seconds to the timer
+    quiz2();
 }
+function advance2() {
+    correctAnswer();
+    quiz3();
+}
+function advance3() {
+    correctAnswer();
+    quiz4();
+}
+
+function advance4() {
+    correctAnswer();
+    youWin();
+}
+
 
 function startQuiz() {
     document.getElementById("answers").style.display = "flex"
     document.getElementById("question").style.display = "flex"
     document.getElementById("startQuiz").style.display = "none"
+    document.getElementById("form").style.display = "none";
     document.getElementById("question").textContent = question1;
     document.getElementById("card1").textContent = answer1a;
     document.getElementById("card2").textContent = answer1b;
     document.getElementById("card3").textContent = answer1c;
     document.getElementById("card4").textContent = answer1d;
     score = 0;
+    isWin = false;
     timerCount = 20;
     startTimer();
     card1.addEventListener("click", wrongAnswer);
     card2.addEventListener("click", wrongAnswer);
     card3.addEventListener("click", wrongAnswer);
-    card4.addEventListener("click", quiz2, correctAnswer, resetButton);
+    card4.addEventListener("click", advance1);
 }
 
 
@@ -125,11 +190,11 @@ function quiz2() {
     document.getElementById("card2").textContent = answer2b;
     document.getElementById("card3").textContent = answer2c;
     document.getElementById("card4").textContent = answer2d;
-    card1.addEventListener("click", quiz3, correctAnswer, resetButton);
+    card1.addEventListener("click", advance2);
     card2.addEventListener("click", wrongAnswer);
     card3.addEventListener("click", wrongAnswer);
     card4.addEventListener("click", wrongAnswer);
-    
+
 }
 
 function quiz3() {
@@ -139,13 +204,13 @@ function quiz3() {
     document.getElementById("card3").textContent = answer3c;
     document.getElementById("card4").textContent = answer3d;
     card1.addEventListener("click", wrongAnswer);
-    card2.addEventListener("click", quiz4, correctAnswer, resetButton);
+    card2.addEventListener("click", advance3);
     card3.addEventListener("click", wrongAnswer);
     card4.addEventListener("click", wrongAnswer);
     console.log(score);
 }
 
-function quiz4 () {
+function quiz4() {
     document.getElementById("question").textContent = question4;
     document.getElementById("card1").textContent = answer4a;
     document.getElementById("card2").textContent = answer4b;
@@ -153,7 +218,7 @@ function quiz4 () {
     document.getElementById("card4").textContent = answer4d;
     card1.addEventListener("click", wrongAnswer);
     card2.addEventListener("click", wrongAnswer);
-    card3.addEventListener("click", youWin, correctAnswer, resetButton);
+    card3.addEventListener("click", advance4);
     card4.addEventListener("click", wrongAnswer);
     console.log(score);
 }
@@ -169,11 +234,12 @@ function showHighScores() {
     document.getElementById("quiz-content").style.display = "none";
     document.getElementById("timeLeft").style.display = "none";
     document.getElementById("back-button").style.display = "flex";
+    document.getElementById("player1").style.display = "flex"
     backButton.addEventListener("click", goBack)
 }
 
 function goBack() {
-    this.style.display="none";
+    this.style.display = "none";
     document.getElementById("high-scores").style.display = "none";
     document.getElementById("answers").style.display = "none";
     document.getElementById("question").style.display = "none";
@@ -191,4 +257,3 @@ startButton.addEventListener("click", startQuiz);
 viewScores.addEventListener("click", showHighScores);
 
 
-// var initials = 
